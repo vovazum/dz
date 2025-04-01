f7157219f6c837815264563de05458c368f24695
Zum <vovzum@ya.ru>
2025-04-01 21:34:44 +0500

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin'); // Добавляем плагин
 
 module.exports = {
   entry: './src/index.js',
@@ -7,6 +8,20 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
+  },
+  // Добавляем секцию resolve.fallback
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "url": require.resolve("url/"),
+      "util": require.resolve("util/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "vm": require.resolve("vm-browserify"),
+      "fs": false, // отключаем fs, так как он не нужен в браузере
+      "tty": require.resolve("tty-browserify")
+    }
   },
   module: {
     rules: [
@@ -31,6 +46,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new NodePolyfillPlugin() // Добавляем плагин в секцию plugins
   ],
   devServer: {
     static: './dist',
